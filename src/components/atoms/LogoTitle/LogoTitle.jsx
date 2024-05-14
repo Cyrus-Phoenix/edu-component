b76630a4f1a7bcadd8bd929efaf4e129940622fb
Mikael Wallin <mikael.x.wallin@gmail.com>
2024-05-14 07:51:41 +0200

    Removed code for purpose of students writing it

--- src/components/atoms/LogoTitle/LogoTitle.jsx
+++ src/components/atoms/LogoTitle/LogoTitle.jsx
@@ -1,33 +1,10 @@
 import React from 'react';
 import styles from './LogoTitle.module.css';
 
-const LogoTitle = ({
-                       children,
-                       size = 'medium',
-                       color = '#333',
-                       fontFamily = 'Arial, sans-serif',
-                       verticalAlign = 'center',
-                       horizontalAlign = 'flex-start',
-                       textOrientation = 'horizontal',
-                       className = ''
-                   }) => {
-    const sizeClass = styles[`logoTitle--${size}`];
-    const writingMode = textOrientation === 'vertical' ? 'vertical-rl' : 'horizontal-tb';
+const LogoTitle = () => {
 
     return (
-        <div
-            className={`${styles.logoTitle} ${sizeClass} ${className}`}
-            style={{
-                color,
-                fontFamily,
-                display: 'flex',
-                alignItems: verticalAlign,
-                justifyContent: horizontalAlign,
-                writingMode
-            }}
-        >
-            {children}
-        </div>
+        <div>LogoTitle</div>
     );
 };
 
--- src/stories/LogoImage.stories.jsx
+++ src/stories/LogoImage.stories.jsx
@@ -5,12 +5,6 @@ export default {
     title: 'Components/Atoms/LogoImage',
     component: LogoImage,
     argTypes: {
-        size: {
-            control: 'number',
-            defaultValue: 100,
-        },
-        color: { control: 'color' },
-        className: { control: 'text' },
     },
 };
 
@@ -18,30 +12,4 @@ const Template = (args) => <LogoImage {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-    size: 100,
-    color: '#000',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-    size: 50,
-    color: '#000',
-};
-
-export const Medium = Template.bind({});
-Medium.args = {
-    size: 100,
-    color: '#000',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-    size: 150,
-    color: '#000',
-};
-
-export const CustomColor = Template.bind({});
-CustomColor.args = {
-    size: 100,
-    color: '#FF5733',
 };
